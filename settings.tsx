6693b8e0ecf727ad35b3f3c99321708428c2e17d
C.s.w <104325729+Sol-HQ@users.noreply.github.com>
2024-04-08 10:44:12 -0400

    Update settings.tsx
    from vsc

--- settings.tsx
+++ settings.tsx
@@ -1,14 +1,14 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "OGs", mintText: "Mint .25 SOL Devnet", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "WL", mintText: "Mint .5 SOL Devnet", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "Public", mintText: "Mint .7 SOL Devnet", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "OGs", mintText: "Mint .3 SOL", buttonLabel: "Mint now!", header: "OG Mint" },
+  { label: "WL", mintText: "Mint .4 SOL", buttonLabel: "Mint now!", header: "WL Mint" },
+  { label: "Public", mintText: "Mint .5 SOL", buttonLabel: "Mint now!", header: "Public Mint" },
   { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://arweave.net/CEU6QKws8AdSYYABYn-yOcvUT76VLRgOBT8okcLKDeY?ext=png";
+export const image = "https://arweave.net=png";
 
 //website title
-export const headerText = "I.A. Mint-Call";
+export const headerText = "Quantum Clip Solana";
