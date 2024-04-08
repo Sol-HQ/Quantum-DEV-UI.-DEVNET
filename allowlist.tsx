cbda8e09a59fbdf80c0571b086fb68127eb00a9a
C.s.w <104325729+Sol-HQ@users.noreply.github.com>
2024-04-08 10:03:16 -0400

    Initial commit

---
+++ allowlist.tsx
@@ -0,0 +1,23 @@
+//add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
+//You can also add them here and copy the route from the console log 
+export const allowLists = new Map<string, Array<string>> ([
+    ["OGs",
+    [
+      "HScJWpHByTMVQuvdMFiy9KqyXTkmyV9E2osA6qQTYvGT",
+      "5bj5sYcJQoDeykVX5wVeBaiM5HgV3q7SF1nPt75bHLjX",
+      "78FujgY7qzFx66DgSckizJGdx4advvLQXT98iWWHkvn6",
+      "5PLZSYo7NSkuX7hRXrXNP4WBxz1TiuvGdDAjfSZUKmN8",
+      "5pc9Qe6VXzUYPgzca3TFufUxpeCNFa7oph1E6rukoqMT"
+    ]],
+    ["WL",
+    [
+      "HScJWpHByTMVQuvdMFiy9KqyXTkmyV9E2osA6qQTYvGT",
+      "47W8Mftd359Moi8vpZbSPAXExP9EGtrLGbtdv8HJusNn"
+    ]],
+    ["Public",
+    [
+      "HScJWpHByTMVQuvdMFiy9KqyXTkmyV9E2osA6qQTYvGT",
+      "5bj5sYcJQoDeykVX5wVeBaiM5HgV3q7SF1nPt75bHLjX",
+      "47W8Mftd359Moi8vpZbSPAXExP9EGtrLGbtdv8HJusNn"
+    ]],
+]);
---
+++ settings.tsx
@@ -0,0 +1,14 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "OGs", mintText: "Mint .25 SOL Devnet", buttonLabel: "Mint now!", header: "OG Mint" },
+  { label: "WL", mintText: "Mint .5 SOL Devnet", buttonLabel: "Mint now!", header: "WL Mint" },
+  { label: "Public", mintText: "Mint .7 SOL Devnet", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+];
+
+//header image in the ui. replace with your own
+export const image = "https://arweave.net/CEU6QKws8AdSYYABYn-yOcvUT76VLRgOBT8okcLKDeY?ext=png";
+
+//website title
+export const headerText = "I.A. Mint-Call";
