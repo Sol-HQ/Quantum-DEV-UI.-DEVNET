40d007047c7d237c928a9d7c3435916df4feb03c
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-03-24 18:54:07 -0400

    Update allowlist.tsx

--- allowlist.tsx
+++ allowlist.tsx
@@ -4,14 +4,7 @@ export const allowLists = new Map<string, Array<string>> ([
     ["OGs",
     [
       "5PLZSYo7NSkuX7hRXrXNP4WBxz1TiuvGdDAjfSZUKmN8",
-      "5bj5sYcJQoDeykVX5wVeBaiM5HgV3q7SF1nPt75bHLjX",
-      "Agvso15jRyVS47PiTY1GSj42gXeGXS9pZuKQ7UpcWLzj",
-      "5pc9Qe6VXzUYPgzca3TFufUxpeCNFa7oph1E6rukoqMT"
-    ]],
-    ["WL",
-    [
-      "HScJWpHByTMVQuvdMFiy9KqyXTkmyV9E2osA6qQTYvGT",
-      "47W8Mftd359Moi8vpZbSPAXExP9EGtrLGbtdv8HJusNn"
+     
     ]],
     ["Public",
     [
